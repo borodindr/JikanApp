6d04e6b5240095b1ab04453aafa0192713db8b55
Dmitry Borodin <borodindr@gmail.com>
2021-11-11 11:23:52 -0600

    Initial commit

---
+++ JikanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JikanApp
+//
+//  Created by Dmitry Borodin on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
